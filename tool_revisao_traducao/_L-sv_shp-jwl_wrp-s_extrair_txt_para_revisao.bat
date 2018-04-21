d3e8b14b9be4d3370b99546b17fd20501e2711b5
cfarl <lutitus@gmail.com>
2018-04-20 23:05:10 -0300

    Traduzidos cfarl mgml_strings_us.u16 shp_jwl_str_us.u16 wrp_pnt_list_us.u16

---
+++ tool_revisao_traducao/_L-sv_shp-jwl_wrp-s_extrair_txt_para_revisao.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabela "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "2" "L_sv" "shp_jwl" "wrp_s" 
+pause
+
---
+++ tool_revisao_traducao/_btl_extrair_txt_para_revisao.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabela "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "11" "BTLT"
+pause
+
