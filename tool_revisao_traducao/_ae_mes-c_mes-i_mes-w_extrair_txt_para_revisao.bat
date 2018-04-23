c4ceb37f9cf21dfc0240a9dcf16f9659a735bcf3
Luiz Curado <lutitus@gmail.com>
2018-04-23 12:07:16 -0300

    Atualizada tool para incluir o arquivo SYSTEMMES_us.u16

--- tool_revisao_traducao/_ae_mes-c_mes-i_mes-w_extrair_txt_para_revisao.bat
+++
@@ -1,3 +0,0 @@
-java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "12" "ae" "mes_c" "mes_i" "mes_w" 
-pause
-
