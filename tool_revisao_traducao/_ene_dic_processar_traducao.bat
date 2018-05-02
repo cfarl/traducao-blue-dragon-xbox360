ca765c863af6cc57732002d62cefb52a65a2ffcd
Luiz Curado <lutitus@gmail.com>
2018-05-02 12:07:07 -0300

    Adicionada tool para extrair somente o texto em ingles do ene_dic.

--- tool_revisao_traducao/_ene_dic_processar_traducao.bat
+++ tool_revisao_traducao/_ene_dic_processar_traducao.bat
@@ -1,3 +1,4 @@
-java -cp ..\tools\tool bluedragon.ImportarTextoTabela "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" ".\extraido_revisao.txt" "10" "ene_dic"
+java -cp ..\tools\tool bluedragon.ImportarTextoSomenteTraducao "..\arquivos-traduzidos\ene_dic_us.u16" ".\extraido_revisao.txt" "10" 
+
 pause
 
