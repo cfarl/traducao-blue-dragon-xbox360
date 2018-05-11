40b47d9037ae46c62f2543fa41671c5c75b593d8
Luiz Curado <lutitus@gmail.com>
2018-05-11 14:16:06 -0300

    Traduzida até linha 37 do ItWd_us.u16. O arquivo tem 1.309 linhas.

--- tool_revisao_traducao/_extrair_08_ItWd.bat
+++ tool_revisao_traducao/_extrair_08_ItWd.bat
@@ -1,4 +1,4 @@
-java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "extraido_revisao08.txt" "0,2" "ItWd"
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "extraido_revisao08.txt" "0,2,3" "ItWd"
 
 pause
 
