fe566555176cd1d63528aaedd17e40434fa76c33
Luiz Curado <lutitus@gmail.com>
2018-05-18 14:12:11 -0300

    Adicionada tool para tradução da descrição longa do ItWd_us.u16

---
+++ tool_revisao_traducao/_extrair_08_ItWd_SOMENTE_DESCRICAO_LONGA.bat
@@ -0,0 +1,4 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "extraido_revisao08.txt" "2" "ItWd"
+
+pause
+
