d78c5dc7af416a953086998d6f2a6146bc298e64
cfarl <lutitus@gmail.com>
2018-06-05 00:43:15 -0300

    Adicionada pasta com texturas traduzidas

--- tool_edicao_texturas/__importar_dds_us.bat
+++ tool_edicao_texturas/__importar_dds_us.bat
@@ -31,10 +31,77 @@ java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\rst_cateun_us
 java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\rst_lvup_us.dds _extraido_rst_lvup_us.dds
 java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\tai_plate_us.dds _extraido_tai_plate_us.dds
 
-
-
+rename  _extraido_btlaf_logoref_001_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\btlaf_logoref_002_us.dds  _extraido_btlaf_logoref_002_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\btlaf_logoref_003_us.dds  _extraido_btlaf_logoref_003_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\btlaf_logo_blur_us.dds _extraido_btlaf_logo_blur_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\btlaf_logo_glow_us.dds _extraido_btlaf_logo_glow_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\btlaf_logo_us.dds _extraido_btlaf_logo_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\btl_trance_us.dds _extraido_btl_trance_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\cmn_heada_us.dds _extraido_cmn_heada_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\cmn_hpmp_us.dds _extraido_cmn_hpmp_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\cmn_lay_main01_us.dds _extraido_cmn_lay_main01_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\cmn_lv_us.dds _extraido_cmn_lv_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\cmn_new_us.dds _extraido_cmn_new_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\cmn_next_us.dds _extraido_cmn_next_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\cmn_page_us.dds _extraido_cmn_page_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\gmov_logo_us.dds _extraido_gmov_logo_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\icon_status_02_us.dds _extraido_icon_status_02_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\icon_status_us.dds _extraido_icon_status_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\ic_itmeffect_us.dds _extraido_ic_itmeffect_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mgcg_hp_us.dds _extraido_mgcg_hp_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mgml_rank_us.dds _extraido_mgml_rank_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_critical_us.dds _extraido_mj_critical_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_defend_us.dds _extraido_mj_defend_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_guard_us.dds _extraido_mj_guard_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_miss_us.dds _extraido_mj_miss_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_quick_us.dds _extraido_mj_quick_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_reflection_us.dds _extraido_mj_reflection_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_shell_us.dds _extraido_mj_shell_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_shield_us.dds _extraido_mj_shield_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\mj_slow_us.dds _extraido_mj_slow_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\rst_cateun_us.dds _extraido_rst_cateun_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\rst_lvup_us.dds _extraido_rst_lvup_us.dds
+java -cp ..\tools\tool bluedragon.texturas.Importar    original-us\tai_plate_us.dds _extraido_tai_plate_us.dds
 
 del novas\* /Q
 rmdir novas
 mkdir novas
 move _novo* novas
+
+cd novas
+rename    _novo_btlaf_logoref_001_us.dds  btlaf_logoref_001_us.dds
+rename    _novo_btlaf_logoref_002_us.dds  btlaf_logoref_002_us.dds
+rename    _novo_btlaf_logoref_003_us.dds  btlaf_logoref_003_us.dds
+rename    _novo_btlaf_logo_blur_us.dds  btlaf_logo_blur_us.dds
+rename    _novo_btlaf_logo_glow_us.dds  btlaf_logo_glow_us.dds
+rename    _novo_btlaf_logo_us.dds  btlaf_logo_us.dds
+rename    _novo_btl_trance_us.dds  btl_trance_us.dds
+rename    _novo_cmn_heada_us.dds  cmn_heada_us.dds
+rename    _novo_cmn_hpmp_us.dds cmn_hpmp_us.dds
+rename    _novo_cmn_lay_main01_us.dds  cmn_lay_main01_us.dds
+rename    _novo_cmn_lv_us.dds  cmn_lv_us.dds
+rename    _novo_cmn_new_us.dds  cmn_new_us.dds
+rename    _novo_cmn_next_us.dds  cmn_next_us.dds
+rename    _novo_cmn_page_us.dds  cmn_page_us.dds
+rename    _novo_gmov_logo_us.dds  gmov_logo_us.dds
+rename    _novo_icon_status_02_us.dds  icon_status_02_us.dds
+rename    _novo_icon_status_us.dds  icon_status_us.dds
+rename    _novo_ic_itmeffect_us.dds  ic_itmeffect_us.dds
+rename    _novo_mgcg_hp_us.dds  mgcg_hp_us.dds
+rename    _novo_mgml_rank_us.dds  mgml_rank_us.dds
+rename    _novo_mj_critical_us.dds  mj_critical_us.dds
+rename    _novo_mj_defend_us.dds  mj_defend_us.dds
+rename    _novo_mj_guard_us.dds  mj_guard_us.dds
+rename    _novo_mj_miss_us.dds  mj_miss_us.dds
+rename    _novo_mj_quick_us.dds  mj_quick_us.dds
+rename    _novo_mj_reflection_us.dds  mj_reflection_us.dds
+rename    _novo_mj_shell_us.dds  mj_shell_us.dds
+rename    _novo_mj_shield_us.dds  mj_shield_us.dds
+rename    _novo_mj_slow_us.dds  mj_slow_us.dds
+rename    _novo_rst_cateun_us.dds  rst_cateun_us.dds
+rename    _novo_rst_lvup_us.dds  rst_lvup_us.dds
+rename    _novo_tai_plate_us.dds  tai_plate_us.dds
+
+
+
