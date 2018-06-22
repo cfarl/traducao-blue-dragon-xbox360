75d7d102e5c8ba1ada1cd750bbbfc4bd69844540
Luiz Curado <lutitus@gmail.com>
2018-06-22 15:23:59 -0300

    Feita a revisão ortográfica do ItWd_us.u16

--- tool_revisao_traducao/_extrair_08_ItWd.bat
+++ tool_revisao_traducao/_extrair_08_ItWd.bat
@@ -1,4 +1,3 @@
-java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "extraido_revisao08.txt" "0,3,2" "ItWd"
-
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "extraido_revisao08.txt" "0,3,2" "ItWd" "--extrairSomenteTextoTraduzidoTIRARISSO" 
 pause
 
