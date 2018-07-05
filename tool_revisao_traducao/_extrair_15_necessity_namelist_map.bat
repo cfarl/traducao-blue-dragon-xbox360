44a787cf0a3aca95231d6c542f2d1aa93ef222d2
Luiz Curado <lutitus@gmail.com>
2018-07-05 14:00:01 -0300

    Adicionados novos arquivos para traduzir (ipk necessity)

---
+++ tool_revisao_traducao/_extrair_15_necessity_namelist_map.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao15.txt" "3" "namelist_map_us" 
+pause
+
---
+++ tool_revisao_traducao/_traduzir_15_necessity_namelist_map.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ImportarTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-necessity-traduzidos" ".\extraido_revisao15.txt" "3" "namelist_map_us" 
+pause
+
