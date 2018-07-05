6129d1c2c9c5dbd9ad9aaa3f78504ab6cae6b099
Luiz Curado <lutitus@gmail.com>
2018-07-05 14:16:43 -0300

    Adicionada tool para traducao de dois arquivos do ipk necessity.

---
+++ tool_revisao_traducao/_extrair_16_necessity_sdw_fix_new_us.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao16.txt" "0,1" "sdw_fix_new_us" 
+pause
+
--- tool_revisao_traducao/_traduzir_15_necessity_namelist_map.bat
+++ tool_revisao_traducao/_traduzir_15_necessity_namelist_map.bat
@@ -1,3 +1,3 @@
-java -cp ..\tools\tool bluedragon.ImportarTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-necessity-traduzidos" ".\extraido_revisao15.txt" "3" "namelist_map_us" 
+java -cp ..\tools\tool bluedragon.ImportarTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" ".\extraido_revisao15.txt" "3" "namelist_map_us" 
 pause
 
