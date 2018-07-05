fa02f91112938c1c1a07ef952321fd63e8e426ee
cfarl <lutitus@gmail.com>
2018-07-05 17:58:39 -0300

    Adicionado mais um arquivo para tradução

---
+++ tool_revisao_traducao/_extrair_17_necessity_common.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao17.txt" "1" "common" 
+pause
+
