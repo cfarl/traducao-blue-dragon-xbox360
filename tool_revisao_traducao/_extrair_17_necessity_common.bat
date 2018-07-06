e2912c0f569eefa574d8b3e0f5a36a332a071543
cfarl <lutitus@gmail.com>
2018-07-06 16:53:54 -0300

    Traduzido common_us.u16

--- tool_revisao_traducao/_extrair_17_necessity_common.bat
+++ tool_revisao_traducao/_extrair_17_necessity_common.bat
@@ -1,3 +1,3 @@
-java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao17.txt" "1" "common" 
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao17.txt" "1" "common" 
 pause
 
