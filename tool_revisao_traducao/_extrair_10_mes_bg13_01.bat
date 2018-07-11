edcebae4ea42a8e3c333515c191a094078af2cac
Luiz Curado <lutitus@gmail.com>
2018-07-11 16:14:21 -0300

    Tools para revisão do arquivo mes_bg13_01_us.u16

---
+++ tool_revisao_traducao/_extrair_10_mes_bg13_01.bat
@@ -0,0 +1,4 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" "extraido_revisao10_13_01.txt" "12" "mes_bg13_01" 
+pause
+
+
---
+++ tool_revisao_traducao/_traduzir_10_mes_bg13_01.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ImportarTextoTabela "..\arquivos-originais-es" "..\arquivos-originais-us" "..\arquivos-traduzidos" ".\extraido_revisao10_13_01.txt" "12" "mes_bg13_01" 
+pause
+
