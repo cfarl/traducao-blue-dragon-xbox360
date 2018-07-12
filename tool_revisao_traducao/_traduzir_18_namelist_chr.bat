e4a77e23223a8e04c59e1213452561ac538d8846
cfarl <lutitus@gmail.com>
2018-07-11 23:11:04 -0300

    Adicionada tool 18

---
+++ tool_revisao_traducao/_traduzir_18_namelist_chr.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ImportarTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" ".\extraido_revisao18.txt" "2" "namelist_chr" 
+pause
+
