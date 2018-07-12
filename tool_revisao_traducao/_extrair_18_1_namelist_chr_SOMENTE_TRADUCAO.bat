b0041085fbc8d0d71d908357317241e47bdca7f2
Luiz Curado <lutitus@gmail.com>
2018-07-12 11:00:53 -0300

    Atualizada tool 18

---
+++ tool_revisao_traducao/_extrair_18_1_namelist_chr_SOMENTE_TRADUCAO.bat
@@ -0,0 +1,3 @@
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabelaSemTratarRepeticao "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao18_1.txt" "2" "namelist_chr" --extrairSomenteTextoTraduzido
+pause
+
---
+++ tool_revisao_traducao/_traduzir_18_1_namelist_chr_SOMENTE_TRADUCAO.bat
@@ -0,0 +1,4 @@
+java -cp ..\tools\tool bluedragon.ImportarTextoSomenteTraducao "..\arquivos-traduzidos-necessity\namelist_chr_us.u16" ".\extraido_revisao18_1.txt" "2" 
+
+pause
+
