fef1855e50c59740fcffd32b2fb19c96b7ccd87a
Luiz Curado <lutitus@gmail.com>
2018-07-12 17:20:55 -0300

    Feita revisão ortográfica dos arquivos das tools 15, 16 e 17.

--- tool_revisao_traducao/_extrair_16_necessity_sdw_fix_new_us.bat
+++ tool_revisao_traducao/_extrair_16_necessity_sdw_fix_new_us.bat
@@ -1,3 +1,1 @@
-java -cp ..\tools\tool bluedragon.ExtrairTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao16.txt" "0,1" "sdw_fix_new_us" 
-pause
-
+java -cp ..\tools\tool bluedragon.ExtrairTextoTabela "..\arquivos-originais-necessity-es" "..\arquivos-originais-necessity-us" "..\arquivos-traduzidos-necessity" "extraido_revisao16.txt" "0,1" "sdw_fix_new_us"  
